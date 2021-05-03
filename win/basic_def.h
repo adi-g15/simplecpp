ace399be607c10459494558c393f484da6b5de98
Aditya Gupta <ag15035@gmail.com>
2021-05-03 22:53:47 +0530

    Added windows version

---
+++ win/basic_def.h
@@ -0,0 +1,31 @@
+//These definitions are included only in windows version of code.
+#ifndef _BASIC_DEF_INCLUDED_
+#define _BASIC_DEF_INCLUDED_
+
+typedef struct{
+          short x,y;
+      }XPoint;
+
+typedef struct {
+	int type;		/* ButtonPress or ButtonRelease */
+	int x, y;		/* pointer x, y coordinates in event window */
+} XButtonEvent;
+
+typedef struct {
+	int type;		/* ButtonPress or ButtonRelease */
+	int x, y;		/* pointer x, y coordinates in event window */
+} XMotionEvent;
+
+ typedef struct {
+	int type;		/* KeyPress or KeyRelease */
+	int x, y;		/* pointer x, y coordinates in event window */
+}XKeyEvent;
+
+typedef struct _XEvent {
+	int type;	/* must not be changed */
+	XKeyEvent xkey; //members initialized to zero;
+	XButtonEvent xbutton;
+	XMotionEvent xmotion;
+} XEvent;
+
+#endif
